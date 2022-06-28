e09e7ee5ff2c04d47c3bd933ee7c861882555396
YahyaBadawy <yahia.badawi@gmail.com>
2022-06-28 08:00:20 +0200

    new

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export AWS_BUCKET="test-501662002263-dev"
+export POSTGRESS_DB="opsazffs"
+export POSTGRESS_PASSWORD="XsWE_QJpd_JN5bZ3FWnIF2WTn3JtIncP"
+export POSTGRESS_HOST="fanny.db.elephantsql.com"
+export AWS_REGION="me-south-1"
+export POSTGRESS_USERNAME="opsazffs"
+export AWS_PROFILE="default"
+export JWT_SECRET="test"
+export URL="http://localhost:8100/"
